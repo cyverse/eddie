d117d9aad6e0b1cd856990ed20cb93d3f08e6a45
Edwin Skidmore <edwins@arizona.edu>
2024-02-28 20:42:29 -0700

    added cacao metadata

--- inputs.tf
+++ inputs.tf
@@ -8,3 +8,28 @@ variable "cyverse_asset_config_dir" {
   description = "string, path to the cyverse asset configuration directory"
 }
 
+# The following is minimally required for cacao
+variable "username" {
+  type        = string
+  description = "name of user"
+}
+
+# this is minimumally needed
+variable "instance_name" {
+  type = string
+  description = "name of instance"
+}
+
+# this is needed for ionosphere
+variable "power_state" {
+  type = string
+  description = "power state of instance"
+  default = "active"
+}
+
+# this is needed to render in ionosphere
+variable "project" {
+  type = string
+  description = "project name"
+  default = ""
+}
